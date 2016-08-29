aac85c7b8bba1cef2e8e1d2efadb8ad01c551337
chris-shum <shum.chris8@gmail.com>
2016-08-29 00:29:30 -0400

    Step 1 done

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,13 +7,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.udacity.gradle.jokes.Joker;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    Joker joker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        joker = new Joker();
+
+
     }
 
 
@@ -40,7 +47,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, joker.getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
