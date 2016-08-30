6c8285dd49b2adde2fef58ce1f4ef2fd0aa88a3b
chris-shum <shum.chris8@gmail.com>
2016-08-30 12:44:19 -0400

    Step 2

---
+++ mylibrary/src/main/java/app/com/example/android/mylibrary/JokeActivity.java
@@ -0,0 +1,22 @@
+package app.com.example.android.mylibrary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class JokeActivity extends AppCompatActivity {
+
+    TextView jokeTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+
+        jokeTextView = (TextView) findViewById(R.id.joke_text_view);
+        Intent intent = getIntent();
+        jokeTextView.setText(intent.getStringExtra("Joke"));
+
+    }
+}
