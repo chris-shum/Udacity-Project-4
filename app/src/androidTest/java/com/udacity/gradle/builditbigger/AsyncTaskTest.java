82134ad75c3cd1ad1ce3ea46d8604bee93bd1ee2
chris-shum <shum.chris8@gmail.com>
2016-09-01 00:40:58 -0400

    Tweaks

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -2,8 +2,6 @@ package com.udacity.gradle.builditbigger;
 
 import org.junit.Test;
 
-import java.util.concurrent.TimeUnit;
-
 import static org.junit.Assert.assertNotNull;
 
 
@@ -16,9 +14,9 @@ public class AsyncTaskTest {
 
     @Test
     public void testAsyncTask() throws Exception {
-        EndpointsAsyncTask blah = new EndpointsAsyncTask();
-        blah.execute();
-        String joke = blah.get(30, TimeUnit.SECONDS);
+        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask();
+        endpointsAsyncTask.execute();
+        String joke = endpointsAsyncTask.get();
         assertNotNull(joke);
 
     }
